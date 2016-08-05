467cb61bfe57b13f20ce74ee2d0a01b1771ad946
Christopher Bross <christopher.bross@fau.de>
2016-08-05 18:39:34 +0200

    some gerneral changes, still bugy

--- multi_export.user.js
+++ multi_export.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @id             iitc-plugin-gpx-exporter
-// @name           IITC plugin: Ingress GPX Exporter
-// @category       Keys
+// @id             iitc-plugin-multie-exporter
+// @name           IITC plugin: Ingress Multi Exporter
+// @category       Misc
 // @version        0.1
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
-// @description    Exports portals currently in view as a GPX list
+// @description    Exports portals information from Bookmarks or from Portals currenntly in few
 // @include        https://www.ingress.com/intel*
 // @include        http://www.ingress.com/intel*
 // @match          https://www.ingress.com/intel*
@@ -19,77 +19,93 @@ function wrapper() {
     }
 
 
+/*********** MENUE ************************************************************/
     window.plugin.createmenu = function() {
         window.dialog({
             title: "Multi Export Options",
             html: '<div class="multiExportSetbox">'
-            + "<a onclick=\"window.plugin.gpxexport.gen();\" title=\"Generate a GPX list of portals and location\">GPX Export</a>"
-            + "<a onclick=\"window.plugin.ingressdualmap.gen();\" title=\"Generate a CSV list of portals and locations\">CSV Export</a>"
+            + "<a onclick=\"window.plugin.gpxexport();\" title=\"Generate a GPX list of portals and location\">GPX Export</a>"
+            + "<a onclick=\"window.plugin.csvexport();\" title=\"Generate a CSV list of portals and locations\">CSV Export</a>"
             + "<a onclick=\"window.plugin.maxfields();\" title=\"Generate a list of portals and locations for use with maxfield\">Maxfields Export</a>"
             + "</div>"
         }).parent();
         // width first, then centre
     };
 
-    window.plugin.maxfields = function() {
+/*********** MAX FIELD on BOOKMARK ********************************************/
+    //TODO menu to chuse bookmarks
+    //TODO add number of keys
+    window.plugin.maxfields = function()
+    {
         var o = [];
         var bookmarks = JSON.parse(localStorage[plugin.bookmarks.KEY_STORAGE]);
-        for(var i in bookmarks.portals.idOthers.bkmrk){
+        for(var i in bookmarks.portals.idOthers.bkmrk)
+        {
             var name = bookmarks.portals.idOthers.bkmrk[i].label;
             var gpscoord = bookmarks.portals.idOthers.bkmrk[i].latlng;
+
             o.push(name + ";https://www.ingress.com/intel?ll=" + gpscoord + "&z=18&pll=" + gpscoord + ";0");
         }
 
         var dialog = window.dialog({
-            title: "Maxfields Export",
-            html: '<span>Use the list bellow as input for maxfields.</span>' + '<textarea id="idmGPXExport" style="width: 600px; height: ' + ($(window).height() - 150) + 'px; margin-top: 5px;"></textarea>'
+            title: "Maxfields Export from Bookmarks",
+            html: '<span>Use the list bellow as input for maxfields.</span>'
+                + '<textarea id="idmExport" style="width: 600px; height: ' + ($(window).height() - 150) + 'px; margin-top: 5px;"></textarea>'
         }).parent();
         $(".ui-dialog-buttonpane", dialog).remove();
-        // width first, then centre
         dialog.css("width", 630).css({
             "top": ($(window).height() - dialog.height()) / 2,
             "left": ($(window).width() - dialog.width()) / 2
         });
-        $("#idmGPXExport").val(o.join("\n"));
+        $("#idmExport").val(o.join("\n"));
         return dialog;
     };
 
-    // base context for plugin
-    window.plugin.gpxexport = function() {};
-    var self = window.plugin.gpxexport;
-    self.gen = function gen() {
+/*********** GPX on Map *******************************************************/
+    window.plugin.gpxexport = function() {
         var o = [];
         o.push("<?xml version=\"1.0\" encoding=\"UTF-8\"?>");
         o.push("<gpx xmlns=\"http://www.topografix.com/GPX/1/0\"  version=\"1.0\" creator=\"Dom\">");
-        for (var x in window.portals) {
+        for (var x in window.portals)
+        {
             var p = window.portals[x];
             var b = window.map.getBounds();
+
             // skip if not currently visible
             if (p._latlng.lat < b._southWest.lat || p._latlng.lng < b._southWest.lng || p._latlng.lat > b._northEast.lat || p._latlng.lng > b._northEast.lng) continue;
+
             // Microdegrees conversion - added by trefmanic
             lat = p._latlng.lat;
             lng = p._latlng.lng;
-            o.push("<wpt lat=\""+ lat + "\" lon=\""  + lng + "\"><name>" + p.options.data.title + "</name></wpt>");
+            name = p.options.data.title;
+
+            o.push("<wpt lat=\""+ lat + "\" lon=\""  + lng + "\">"
+                    +"<name>" + name "</name>"
+                    //+"<desc>" + "Lat/Lon: " + lat + " " + lng + ""
+                              //+ "Intel: " + "https://www.ingress.com/intel?ll=" + lat +"," + lng +"&z=17&pll" + lat + "," + lng
+                              //+ "GMap: " + "http://maps.google.com/?ll=" + lat +"," + lng +"&q=" + lat +"," lng
+                    //+"</desc>"
+                 + "</wpt>");
         }
         o.push("</gpx>");
 
         var dialog = window.dialog({
-            title: "GPX Export",
-            html: '<span>Save the list below as a GPX file.</span>' + '<textarea id="idmGPXExport" style="width: 600px; height: ' + ($(window).height() - 150) + 'px; margin-top: 5px;"></textarea>'
+            title: "GPX Export from Map",
+            html: '<span>Save the list below as a GPX file.</span>'
+                + '<textarea id="idmExport" style="width: 600px; height: ' + ($(window).height() - 150) + 'px; margin-top: 5px;"></textarea>'
         }).parent();
         $(".ui-dialog-buttonpane", dialog).remove();
-        // width first, then centre
         dialog.css("width", 630).css({
             "top": ($(window).height() - dialog.height()) / 2,
             "left": ($(window).width() - dialog.width()) / 2
         });
-        $("#idmGPXExport").val(o.join("\n"));
+        $("#idmExport").val(o.join("\n"));
         return dialog;
     };
 
-    window.plugin.ingressdualmap = function() {};
-    var self = window.plugin.ingressdualmap;
-    self.gen = function gen() {
+/*********** CSV on Map *******************************************************/
+    //TODO broken
+    window.plugin.csvexport = function() {
         var o = [];
         for (var x in window.portals) {
             var p = window.portals[x];
@@ -105,8 +121,7 @@ function wrapper() {
 
         var dialog = window.dialog({
             title: "Ingress CSV export",
-            html: '<span>Save the list below as a CSV file, then use it with maxfield script.</span>'
-            + '<textarea id="idmCSVExport" style="width: 600px; height: ' + ($(window).height() - 150) + 'px; margin-top: 5px;"></textarea>'
+            html: '<span>Save the list below as a CSV file.</span>' + '<textarea id="idmGPXExport" style="width: 600px; height: ' + ($(window).height() - 150) + 'px; margin-top: 5px;"></textarea>'
         }).parent();
         $(".ui-dialog-buttonpane", dialog).remove();
         // width first, then centre
@@ -114,11 +129,11 @@ function wrapper() {
             "top": ($(window).height() - dialog.height()) / 2,
             "left": ($(window).width() - dialog.width()) / 2
         });
-        $("#idmCSVExport").val(o.join("\n"));
+        $("#idmGPXExport").val(o.join("\n"));
         return dialog;
     };
 
-
+/*********** PLUGIN SETUP *****************************************************/
     // setup function called by IITC
     self.setup = function init() {
         // add controls to toolbox
@@ -129,6 +144,7 @@ function wrapper() {
         // delete setup to ensure init can't be run again
         delete self.setup;
     };
+
     // IITC plugin setup
     if (window.iitcLoaded && typeof self.setup === "function") {
         self.setup();
@@ -138,6 +154,9 @@ function wrapper() {
         window.bootPlugins = [self.setup];
     }
 }
+
+
+
 // inject plugin into page
 var script = document.createElement("script");
 script.appendChild(document.createTextNode("(" + wrapper + ")();"));
