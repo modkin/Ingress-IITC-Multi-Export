5eaf7dd643a5995ed12e029b778b992404914e63
Christopher Bross <christopher.bross@fau.de>
2017-01-30 23:43:33 +0100

    add wildcard in import and natch url

--- multi_export.user.js
+++ multi_export.user.js
@@ -2,15 +2,19 @@
 // @id             iitc-plugin-portal-multi-export
 // @name           IITC plugin: Portal Multi Export
 // @category       Misc
-// @version        0.4
+// @version        0.5
 // @namespace      https://github.com/jonatkins/ingress-intel-total-conversion
 // @updateURL      https://github.com/modkin/Ingress-IITC-Multi-Export/raw/master/multi_export.user.js
 // @downloadURL    https://github.com/modkin/Ingress-IITC-Multi-Export/raw/master/multi_export.user.js
 // @description    Export portals from bookmarks, current view or polygon
-// @include        https://www.ingress.com/intel*
-// @include        http://www.ingress.com/intel*
-// @match          https://www.ingress.com/intel*
-// @match          http://www.ingress.com/intel*
+// @include        https://*.ingress.com/intel*
+// @include        http://*.ingress.com/intel*
+// @include        https://*.ingress.com/mission*
+// @include        http://*.ingress.com/mission*
+// @match          https://*.ingress.com/intel*
+// @match          http://*.ingress.com/intel*
+// @match          https://*.ingress.com/mission*
+// @match          http://*.ingress.com/mission*
 // @grant          none
 // ==/UserScript==
 
